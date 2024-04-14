43aaef92b11214671d4c203de5bd0901b9a9a256
Deepikamt <113481686+Deepikamt@users.noreply.github.com>
2024-04-14 13:23:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,22 @@
 import psycopg2
 from flask import Flask
 import os
-
-# Set variables equal to connections
-dbname = 'gis5572'
-user = 'postgres'
-password = 'Deepika@98'
-host = '35.221.47.162'  # Cloud DB Public IP address
-port = '5432'
-
 # create the Flask app
-app = Flask(__name__) 
-
+app = Flask(__name__)
 # Connect to the PostgreSQL database
-
 # create the index route
-@app.route('/') 
-def index(): 
+@app.route('/')
+def index():
     return "The API is working!"
-
 # create a general DB to GeoJSON function
 def database_to_geojson(table_name):
         # create connection to the DB
     conn = psycopg2.connect(
-        host = os.environ.get("DB_HOST"),
-        database = os.environ.get("DB_NAME"),
-        user = os.environ.get("DB_USER"),
-        password = os.environ.get("DB_PASS"),
-        port = os.environ.get("DB_PORT"),
+        dbname = 'gis5572'
+        user = 'postgres'
+        password = 'Deepika@98'
+        host = '35.221.47.162'  # Cloud DB Public IP address
+        port = '5432'
     )
     # retrieve the data
     with conn.cursor() as cur:
@@ -40,24 +29,27 @@ def database_to_geojson(table_name):
         )
         FROM {table_name};
         """
-        
         cur.execute(query)
-        
         data = cur.fetchall()
     # close the connection
     conn.close()
-    
     # Returning the data
     return data [0][0]
-
 # create the data route
-
 @app.route('/get_elevation_idw_geojson', methods=['GET'])
 def get_elevation_idw_geojson():
     # call our general function
-    ele_idw = idwelevationpoints_in_sde 
+    ele_idw = database_to_geojson("idwelevationpoints_in_sde")
     return ele_idw
-
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+
+
+
+
+
+
+
+
+
